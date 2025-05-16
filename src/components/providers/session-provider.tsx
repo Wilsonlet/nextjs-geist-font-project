521d8d6f90e2ca0f983bbc1096d9dbc1b07a15d7
Wilsonlet <52114799+Wilsonlet@users.noreply.github.com>
2025-05-16 02:15:30 +0100

    Add src/components/providers/session-provider.tsx 1747358126615

---
+++ src/components/providers/session-provider.tsx
@@ -0,0 +1,9 @@
+import { SessionProvider } from "next-auth/react"
+
+type Props = {
+  children: React.ReactNode
+}
+
+export function AuthProvider({ children }: Props) {
+  return <SessionProvider>{children}</SessionProvider>
+}
